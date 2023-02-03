1b989e3129cb845dd4a57e2dc0e74dd97b7676bc
Mohammed Shemim <121789121+MohammedShemim@users.noreply.github.com>
2023-02-03 16:10:05 +0300

    Create Power_of_three.js

---
+++ Recursion/Power_of_three.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfThree = function(n) {
+    if(n==1)
+return true;
+if(n>1)
+return n%3==0 && isPowerOfThree(n/3);
+else
+return false;
+};
