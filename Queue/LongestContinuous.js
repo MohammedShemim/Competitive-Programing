8125500c8ca29489f7d5ddfec9bb3070cbba543d
Mohammed Shemim <121789121+MohammedShemim@users.noreply.github.com>
2023-02-13 17:41:53 +0300

    Create LongestContinuous.js

---
+++ Queue/LongestContinuous.js
@@ -0,0 +1,40 @@
+const longestSubarray = (nums, limit) => {
+    let i = 0;
+    let j = 0;
+    let max = 0;
+    
+    const maxQ = [];
+    const minQ = [];
+    
+    while (j < nums.length) {
+        let num = nums[j];
+        
+        while (maxQ.length && nums[maxQ[maxQ.length - 1]] <= num) {
+            maxQ.pop();
+        }
+    
+         while (minQ.length && nums[minQ[minQ.length - 1]] >= num) {
+            minQ.pop();
+        }
+        
+        maxQ.push(j);
+        minQ.push(j);
+        
+        if(nums[maxQ[0]] - nums[minQ[0]] > limit) {
+            i++;
+            
+            if (i > maxQ[0]) {
+                maxQ.shift();
+            }
+            
+            if(i > minQ[0]) {
+                minQ.shift();
+            }
+        } else {
+           max = Math.max(max, j - i + 1); 
+                      j++;
+        }
+    }
+    
+    return max;
+};
